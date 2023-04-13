3a46f7bd903367e15f317c35825dd1009419f8db
IgorD934 <igord934@gmail.com>
2023-04-13 12:02:54 -0300

    Add libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 // import "./App.css";
 
-import { Box, Button } from "@chakra-ui/react";
+import { AspectRatio, Box, Button, Container } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box
-      h={"100vh"}
-      w={"100vw"}
-      display={"flex"}
-      alignContent={"center"}
-      alignItems={"center"}
-      justifyContent={"space-between"}
-      bg={"#F6f6f6"}
-    >
-      <Button variant={"light"}>Text Button</Button>
-
-      <Button variant={"brand1"}>Text Button</Button>
-      <Button variant={"brand1"} size={"big"}>
-        Text Button
-      </Button>
-    </Box>
+    <AspectRatio maxW="1024px" ratio={1} margin={"0 auto"} h={"100vh"}>
+      <iframe
+        title="naruto"
+        src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+        allowFullScreen
+      />
+    </AspectRatio>
   );
 }
 
