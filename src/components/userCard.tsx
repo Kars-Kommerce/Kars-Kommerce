259857e92d1594e0011e5e0b2beab42bddde7e9d
Igorttdp <igorttdp.dev@email.com>
2023-04-17 12:05:55 -0300

    feat: create a 'UserCard' sub-component for 'ProductCard' component

---
+++ src/components/userCard.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const UserCardContainer = styled.div`
+  > div {
+    width: 32px;
+    height: 32px;
+    background-color: #4529e6;
+    color: #fff;
+    font-size: 14px;
+    border-radius: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+interface UserCardProps {
+  authorName: string;
+}
+
+const UserCard = ({ authorName }: UserCardProps) => {
+  const abbreviateName = () => {
+    const splited = authorName.split(" ");
+
+    if (splited.length > 1) return `${splited[0][0]}${splited[1][0]}`;
+
+    return `${splited[0][0]}`;
+  };
+
+  return (
+    <UserCardContainer>
+      <div>{abbreviateName()}</div>
+      <span>{authorName}</span>
+    </UserCardContainer>
+  );
+};
+
+export default UserCard;
