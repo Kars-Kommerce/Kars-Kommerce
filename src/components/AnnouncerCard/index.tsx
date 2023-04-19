c3440b62a2fc59775f7d8224f38f78b253faa4fd
IgorD934 <igord934@gmail.com>
2023-04-19 14:36:23 -0300

    Fix AnnouncerCard

---
+++ src/components/AnnouncerCard/index.tsx
@@ -0,0 +1,60 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Text,
+  Avatar,
+  Link,
+} from "@chakra-ui/react";
+const AnnouncerCard = ({ authorName, description, link }: any) => {
+  return (
+    <>
+      <Card
+        w={"90%"}
+        maxW="md"
+        css={{ boxShadow: "none" }}
+        borderRadius={"4px"}
+        bg={"grey.white"}
+        padding={"15px 0"}
+      >
+        <CardBody>
+          <Stack css={{ display: "flex", alignItems: "center" }}>
+            <Avatar
+              color={"grey.white"}
+              size="xl"
+              name={authorName}
+              bgColor={"brand.1"}
+            ></Avatar>
+          </Stack>
+
+          <Stack mt="6" spacing="7">
+            <Heading size="md" fontSize="h6" textAlign={"center"} noOfLines={1}>
+              {authorName}{" "}
+            </Heading>
+            <Text textAlign={"center"} color={"grey.2"} noOfLines={3}>
+              {description}
+            </Text>
+          </Stack>
+        </CardBody>
+
+        <CardFooter display={"flex"} justifyContent={"center"}>
+          <Link
+            variant="solid"
+            bg={"grey.0"}
+            color={"grey.white"}
+            _hover={{ bg: "grey.2" }}
+            padding={"12px 24px"}
+            borderRadius={"4px"}
+            href={link}
+          >
+            Ver todos anuncios
+          </Link>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
+
+export default AnnouncerCard;
