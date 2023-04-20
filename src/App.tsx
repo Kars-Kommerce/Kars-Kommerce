cb1df04a21c60b891723d14d4d326ed8f7bc2faf
unknown <leo_costab@outlook.com>
2023-04-20 15:53:15 -0300

    create: login Page and login form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import ListProductsCard from "./components/ListProductCard/listProductCard";
 import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
@@ -14,10 +16,12 @@ function App() {
       flexDirection={"column"}
       justifyContent={"space-between"}
       padding={0}
+      bg={'grey.8'}
     >
       <Navbar />
-      <Banner image={"/src/assets/background_car.png"} />
-      <ListProductsCard />
+      {/* <Banner image={"/src/assets/background_car.png"} />
+      <ListProductsCard /> */}
+      <RegisterForm></RegisterForm>
       <Footer></Footer>
     </Container>
   );
