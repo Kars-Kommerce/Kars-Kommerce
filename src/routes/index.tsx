c86e7e116b41c3360813ba577e532f93aaa9cdeb
unknown <leo_costab@outlook.com>
2023-05-01 15:50:46 -0300

    add: modal create ad

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AnnouncerProfile from '../pages/AnnouncerProfile';
+import ModalCreateAd from '../components/Modal/CreateAd/ModalCreateAd';
 
 const RoutesMain = () => (
   <Router>
     <Routes>
       <Route path="/profile" element={<AnnouncerProfile></AnnouncerProfile>} />
+      <Route path="/teste" element={<ModalCreateAd></ModalCreateAd>} />
     </Routes>
   </Router>
 );
