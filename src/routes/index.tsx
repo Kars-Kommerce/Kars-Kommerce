6f9c37eb43cb99c4977460fa43c03dd90be94259
unknown <leo_costab@outlook.com>
2023-05-01 16:17:29 -0300

    create: base edit profile modal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AnnouncerProfile from '../pages/AnnouncerProfile';
 import ModalCreateAd from '../components/Modal/CreateAd/ModalCreateAd';
+import ModalEditProfile from '../components/Modal/EditProfile/ModalEditProfile';
 
 const RoutesMain = () => (
   <Router>
     <Routes>
       <Route path="/profile" element={<AnnouncerProfile></AnnouncerProfile>} />
-      <Route path="/teste" element={<ModalCreateAd></ModalCreateAd>} />
+      <Route path="/teste" element={<ModalEditProfile></ModalEditProfile>} />
     </Routes>
   </Router>
 );
